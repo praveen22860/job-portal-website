2e9fe5c442e533db067af63fedefaa184ca7ccb8
setty praveen kumar <praveensetty401@gmail.com>
2026-05-31 13:35:13 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,17 +12,17 @@ app.listen(process.env.PORT, () => {
 });
 
 
-const express = require('express');
-const cors = require('cors');
-const app1 = express();
+// const express = require('express');
+// const cors = require('cors');
+// const app1 = express();
 
-// Use the PORT provided by Render or fallback to 5000 locally
-const PORT = process.env.PORT || 5000; 
+// // Use the PORT provided by Render or fallback to 5000 locally
+// const PORT = process.env.PORT || 5000; 
 
-app1.use(cors({
-  origin: process.env.FRONTEND_URL, // This environment variable will be set in Render
-  credentials: true
-}));
+// app1.use(cors({
+//   origin: process.env.FRONTEND_URL, // This environment variable will be set in Render
+//   credentials: true
+// }));
 
 // Ensure your database connection references process.env.DATABASE_URL or process.env.MONGO_URI
 
